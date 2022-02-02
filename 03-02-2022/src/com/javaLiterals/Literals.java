9fc9424940314650fd68211d6738d5155082effe
PC <PC@DESKTOP-JJS8DL4>
2022-02-03 02:56:49 +0530

    java program pushed to 03-02-2022

---
+++ 03-02-2022/src/com/javaLiterals/Literals.java
@@ -0,0 +1,14 @@
+//user-defined package javaLiterals
+package com.javaLiterals;
+// user-defined class name
+public class Literals {
+
+	public static void main(String[] args) {
+		//java program to print hello java
+		System.out.println("Hello Java");
+
+		
+
+	}
+
+}
